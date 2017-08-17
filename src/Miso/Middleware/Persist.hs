92a2209c7113416a6ce4beac4ac73b3482ca4337
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2017-08-17 18:22:47 +0200

    Avoid having to compare models before persisting

--- src/Miso/Middleware/Persist.hs
+++ src/Miso/Middleware/Persist.hs
@@ -13,6 +13,7 @@ import Miso.Middleware.Internal
 data PersistAction model action
  = InitAction
  | LoadedModel !model
+ | PersistedNoop
  | OtherAction !action
  deriving (Show, Eq, Ord)
 
@@ -23,27 +24,20 @@ loadModelWithDefault defaultModel = do
     Left err -> putStrLn ("Couldn’t load model from local storage: " <> err) >> pure defaultModel
     Right model -> pure model
 
-withPersistentModel :: (Eq model, ToJSON model, FromJSON model) => action -> App model action -> App model (PersistAction model action)
-withPersistentModel persistAction (App model update view subs events initialAction) =
+withPersistentModel :: (Eq model, ToJSON model, FromJSON model) => App model action -> App model (PersistAction model action)
+withPersistentModel (App model update view subs events initialAction) =
   App model update' view' subs' events InitAction
   where
     update' InitAction model =
       model <# (LoadedModel <$> loadModelWithDefault model)
+    update' PersistedNoop model = noEff model
     update' (LoadedModel model) _ = model <# pure (OtherAction initialAction)
     update' (OtherAction action) model =
       case update action model of
-        Effect model' acts
--- We need to check that the model has actually changed, otherwise we
--- get stuck in an infinite loop since after updating local storage,
--- 'update' will be called again with 'persistAction' and there is no
--- way to detect that. There are various alternatives to this but I
--- haven’t yet decided which one is best.
-          | model /= model' ->
-            Effect
-              model'
-              ((OtherAction persistAction <$
-                setLocalStorage "miso.persist.model" model') :
-               map (fmap OtherAction) acts)
-          | otherwise -> Effect model' (map (fmap OtherAction) acts)
+        Effect model' acts ->
+          Effect
+            model'
+            ((PersistedNoop <$ setLocalStorage "miso.persist.model" model') :
+             map (fmap OtherAction) acts)
     view' = \model -> fmap OtherAction (view model)
     subs' = map (mapSubAction OtherAction) subs
